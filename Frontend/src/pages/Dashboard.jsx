353794a9a646efe01b1f69769ce4f89fff794d14
jhon-frand <frandlebaza@gmail.com>
2024-03-17 14:22:08 -0500

    conflicts 3

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -8,7 +8,7 @@ import { faToolbox } from '@fortawesome/free-solid-svg-icons'
 function Dashboard() {
   return (
     <>
-      <div className="relative">
+       <div className="relative">
     <div className="absolute top-0 left-0 right-0 z-50">
         <NavBar></NavBar>
     </div>
