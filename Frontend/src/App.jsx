21a5433aa3ed0b7bcc2e8f569d42e700a9253808
alexander2904 <alexjo2903@gmail.com>
2024-03-18 13:29:58 -0500

    confirmacion de funcionamiento

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,22 +1,12 @@
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
-<<<<<<< HEAD
 import Actividades from './components/organismos/templates_alexander/actividades';
 import Tecnicos from './components/organismos/templates_alexander/tecnicos';
-=======
-import UsuariosTemplate from './components/templatesSantiago/UsuariosTemplate';
-import EquiposTemplate from './components/templates/EquiposTemplate';
-import CategoriasTemplate from './components/templates/CategoriasTemplate';
-import UbicacionesTemplate from './components/templates/UbicacionesTemplate';
-import PruebasTemplate from './components/templates/PruebasTemplate';
-import UnidadesTemplate from './components/templates/UnidadesTemplate';
->>>>>>> a42d72e795b019f675e0ef10ef1f45bf5af6f728
 
 function App() {
-  
+  re
   return (
     <>
-<<<<<<< HEAD
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Dashboard/>}/>
@@ -24,19 +14,6 @@ function App() {
         <Route path='/actividades' element={<Actividades/>}/>
       </Routes>
     </BrowserRouter>
-=======
-      <BrowserRouter>
-      <Routes>
-       <Route path='/' element={<Dashboard/>}/>
-       <Route path='/usuarios' element={<UsuariosTemplate/>}/> 
-      <Route path='/equipos' element={<EquiposTemplate/>}/>
-      <Route path='/categorias' element={<CategoriasTemplate/>}/>
-      <Route path='/ubicaciones' element={<UbicacionesTemplate/>}/>
-      <Route path='/pruebas' element={<PruebasTemplate/>}/>
-      <Route path='/unidades' element={<UnidadesTemplate/>}/>
-       </Routes>
-       </BrowserRouter>
->>>>>>> a42d72e795b019f675e0ef10ef1f45bf5af6f728
     </>
   )
 }
